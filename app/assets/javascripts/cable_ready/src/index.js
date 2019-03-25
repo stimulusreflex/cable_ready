53275c66d555f86715318c6b16391f6dd46695cf
Nathan Hopkins <natehop@gmail.com>
2019-03-25 07:17:42 -0600

    Update prettier to use single quotes

--- app/assets/javascripts/cable_ready/src/index.js
+++ app/assets/javascripts/cable_ready/src/index.js
@@ -1,4 +1,4 @@
-import morphdom from "morphdom";
+import morphdom from 'morphdom';
 
 const dispatch = (name, detail = {}) => {
   DOMOperations.dispatchEvent({ name, detail });
@@ -19,113 +19,113 @@ const DOMOperations = {
   // Element Mutations .......................................................................................
 
   morph: config => {
-    let template = document.createElement("template");
+    let template = document.createElement('template');
     template.innerHTML = String(config.html).trim();
-    dispatch("cable-ready:before-morph", { config, content: template.content });
+    dispatch('cable-ready:before-morph', { config, content: template.content });
     morphdom(document.querySelector(config.selector), template.content, {
       childrenOnly: !!config.childrenOnly
     });
     if (config.focusSelector) {
       document.querySelector(config.focusSelector).focus();
     }
-    dispatch("cable-ready:after-morph", { config, content: template.content });
+    dispatch('cable-ready:after-morph', { config, content: template.content });
   },
 
   innerHtml: config => {
-    dispatch("cable-ready:before-inner-html", config);
+    dispatch('cable-ready:before-inner-html', config);
     document.querySelector(config.selector).innerHTML = config.html;
     if (config.focusSelector) {
       document.querySelector(config.focusSelector).focus();
     }
-    dispatch("cable-ready:after-inner-html", config);
+    dispatch('cable-ready:after-inner-html', config);
   },
 
   outerHtml: config => {
-    dispatch("cable-ready:before-outer-html", config);
+    dispatch('cable-ready:before-outer-html', config);
     const element = document.querySelector(config.selector);
     element.outerHTML = config.html;
     if (config.focusSelector) {
       document.querySelector(config.focusSelector).focus();
     }
-    dispatch("cable-ready:after-outer-html", config);
+    dispatch('cable-ready:after-outer-html', config);
   },
 
   textContent: config => {
-    dispatch("cable-ready:before-text-content", config);
+    dispatch('cable-ready:before-text-content', config);
     document.querySelector(config.selector).textContent = config.text;
-    dispatch("cable-ready:after-text-content", config);
+    dispatch('cable-ready:after-text-content', config);
   },
 
   insertAdjacentHtml: config => {
-    dispatch("cable-ready:before-insert-adjacent-html", config);
+    dispatch('cable-ready:before-insert-adjacent-html', config);
     document
       .querySelector(config.selector)
-      .insertAdjacentHTML(config.position || "beforeend", config.html);
+      .insertAdjacentHTML(config.position || 'beforeend', config.html);
     if (config.focusSelector) {
       document.querySelector(config.focusSelector).focus();
     }
-    dispatch("cable-ready:after-insert-adjacent-html", config);
+    dispatch('cable-ready:after-insert-adjacent-html', config);
   },
 
   insertAdjacentText: config => {
-    dispatch("cable-ready:before-insert-adjacent-text", config);
+    dispatch('cable-ready:before-insert-adjacent-text', config);
     document
       .querySelector(config.querySelector)
-      .insertAdjacentText(config.position || "beforeend", config.text);
-    dispatch("cable-ready:after-insert-adjacent-text", config);
+      .insertAdjacentText(config.position || 'beforeend', config.text);
+    dispatch('cable-ready:after-insert-adjacent-text', config);
   },
 
   remove: config => {
-    dispatch("cable-ready:before-remove", config);
+    dispatch('cable-ready:before-remove', config);
     document.querySelector(config.selector).remove();
     if (config.focusSelector) {
       document.querySelector(config.focusSelector).focus();
     }
-    dispatch("cable-ready:after-remove", config);
+    dispatch('cable-ready:after-remove', config);
   },
 
   setValue: config => {
-    dispatch("cable-ready:before-set-value", config);
+    dispatch('cable-ready:before-set-value', config);
     document.querySelector(config.selector).value = config.value;
-    dispatch("cable-ready:after-set-value", config);
+    dispatch('cable-ready:after-set-value', config);
   },
 
   // Attribute Mutations .....................................................................................
 
   setAttribute: config => {
-    dispatch("cable-ready:before-set-attribute", config);
+    dispatch('cable-ready:before-set-attribute', config);
     document
       .querySelector(config.selector)
       .setAttribute(config.name, config.value);
-    dispatch("cable-ready:after-set-attribute", config);
+    dispatch('cable-ready:after-set-attribute', config);
   },
 
   removeAttribute: config => {
-    dispatch("cable-ready:before-remove-attribute", config);
+    dispatch('cable-ready:before-remove-attribute', config);
     document.querySelector(config.selector).removeAttribute(config.name);
-    dispatch("cable-ready:after-remove-attribute", config);
+    dispatch('cable-ready:after-remove-attribute', config);
   },
 
   // CSS Class Mutations .....................................................................................
 
   addCssClass: config => {
-    dispatch("cable-ready:before-add-css-class", config);
+    dispatch('cable-ready:before-add-css-class', config);
     document.querySelector(config.selector).classList.add(config.name);
-    dispatch("cable-ready:after-add-css-class", config);
+    dispatch('cable-ready:after-add-css-class', config);
   },
 
   removeCssClass: config => {
-    dispatch("cable-ready:before-remove-css-class", config);
+    dispatch('cable-ready:before-remove-css-class', config);
     document.querySelector(config.selector).classList.remove(config.name);
-    dispatch("cable-ready:after-remove-css-class", config);
+    dispatch('cable-ready:after-remove-css-class', config);
   },
 
   // Dataset Mutations .......................................................................................
 
   setDatasetProperty: config => {
-    dispatch("cable-ready:before-set-dataset-property", config);
+    dispatch('cable-ready:before-set-dataset-property', config);
     document.querySelector(config.selector).dataset[config.name] = config.value;
-    dispatch("cable-ready:after-set-dataset-property", config);
+    dispatch('cable-ready:after-set-dataset-property', config);
   }
 };
 
--- app/assets/javascripts/cable_ready/src/webpack.config.js
+++ app/assets/javascripts/cable_ready/src/webpack.config.js
@@ -1,23 +1,23 @@
 module.exports = {
-  mode: "production",
-  target: "web",
-  entry: "./index.js",
+  mode: 'production',
+  target: 'web',
+  entry: './index.js',
   plugins: [],
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        loader: "babel-loader",
+        loader: 'babel-loader',
         options: {
-          presets: ["@babel/preset-env"]
+          presets: ['@babel/preset-env']
         }
       }
     ]
   },
   output: {
-    filename: "../../../cable_ready.js",
-    library: "CableReady",
-    libraryTarget: "global"
+    filename: '../../../cable_ready.js',
+    library: 'CableReady',
+    libraryTarget: 'global'
   }
 };
