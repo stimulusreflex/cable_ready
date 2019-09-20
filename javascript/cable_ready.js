0b7781e1442c91072c8546fb113cd8a423168e57
Nate Hopkins <natehop@gmail.com>
2019-09-19 18:30:26 -0600

    Ensure that events bubble

--- javascript/cable_ready.js
+++ javascript/cable_ready.js
@@ -1,5 +1,12 @@
 import morphdom from 'morphdom';
 
+const dispatch = (element, name, detail = {}) => {
+  const init = { bubbles: true, cancelable: true };
+  const evt = new Event(name, init);
+  evt.detail = detail;
+  element.dispatchEvent(evt);
+};
+
 const xpathToElement = xpath => {
   return document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
 };
@@ -9,7 +16,7 @@ const DOMOperations = {
 
   dispatchEvent: config => {
     const { element, name, detail } = config;
-    element.dispatchEvent(new CustomEvent(name, { detail }));
+    dispatch(element, name, detail);
   },
 
   // Element Mutations .......................................................................................
@@ -18,105 +25,104 @@ const DOMOperations = {
     const { element, html, childrenOnly, focusSelector } = detail;
     const template = document.createElement('template');
     template.innerHTML = String(html).trim();
-    detail = { ...detail, content: template.content };
-    element.dispatchEvent(new CustomEvent('cable-ready:before-morph', { detail }));
+    dispatch(element, 'cable-ready:before-morph', { ...detail, content: template.content });
     morphdom(element, template.content, { childrenOnly: !!childrenOnly });
     if (focusSelector) document.querySelector(focusSelector).focus();
-    element.dispatchEvent(new CustomEvent('cable-ready:after-morph', { detail }));
+    dispatch(element, 'cable-ready:after-morph', { ...detail, content: template.content });
   },
 
   innerHtml: detail => {
     const { element, html, focusSelector } = detail;
-    element.dispatchEvent(new CustomEvent('cable-ready:before-inner-html', { detail }));
+    dispatch(element, 'cable-ready:before-inner-html', detail);
     element.innerHTML = html;
     if (focusSelector) document.querySelector(focusSelector).focus();
-    element.dispatchEvent(new CustomEvent('cable-ready:after-inner-html', { detail }));
+    dispatch(element, 'cable-ready:after-inner-html', detail);
   },
 
   outerHtml: detail => {
     const { element, html, focusSelector } = detail;
-    element.dispatchEvent(new CustomEvent('cable-ready:before-outer-html', { detail }));
+    dispatch(element, 'cable-ready:before-outer-html', detail);
     element.outerHTML = html;
     if (focusSelector) document.querySelector(focusSelector).focus();
-    element.dispatchEvent(new CustomEvent('cable-ready:after-outer-html', { detail }));
+    dispatch(element, 'cable-ready:after-outer-html', detail);
   },
 
   textContent: detail => {
     const { element, text } = detail;
-    element.dispatchEvent(new CustomEvent('cable-ready:before-text-content', { detail }));
+    dispatch(element, 'cable-ready:before-text-content', detail);
     element.textContent = text;
-    element.dispatchEvent(new CustomEvent('cable-ready:after-text-content', { detail }));
+    dispatch(element, 'cable-ready:after-text-content', detail);
   },
 
   insertAdjacentHtml: detail => {
     const { element, html, position, focusSelector } = detail;
-    element.dispatchEvent(new CustomEvent('cable-ready:before-insert-adjacent-html', { detail }));
+    dispatch(element, 'cable-ready:before-insert-adjacent-html', detail);
     element.insertAdjacentHTML(position || 'beforeend', html);
     if (focusSelector) document.querySelector(focusSelector).focus();
-    element.dispatchEvent(new CustomEvent('cable-ready:after-insert-adjacent-html', { detail }));
+    dispatch(element, 'cable-ready:after-insert-adjacent-html', detail);
   },
 
   insertAdjacentText: detail => {
     const { element, text, position } = detail;
-    element.dispatchEvent(new CustomEvent('cable-ready:before-insert-adjacent-text', { detail }));
+    dispatch(element, 'cable-ready:before-insert-adjacent-text', detail);
     element.insertAdjacentText(position || 'beforeend', text);
-    element.dispatchEvent(new CustomEvent('cable-ready:after-insert-adjacent-text', { detail }));
+    dispatch(element, 'cable-ready:after-insert-adjacent-text', detail);
   },
 
   remove: detail => {
     const { element, focusSelector } = detail;
-    element.dispatchEvent(new CustomEvent('cable-ready:before-remove', { detail }));
+    dispatch(element, 'cable-ready:before-remove', detail);
     element.remove();
     if (focusSelector) document.querySelector(focusSelector).focus();
-    element.dispatchEvent(new CustomEvent('cable-ready:after-remove', { detail }));
+    dispatch(element, 'cable-ready:after-remove', detail);
   },
 
   setValue: detail => {
     const { element, value } = detail;
-    element.dispatchEvent(new CustomEvent('cable-ready:before-set-value', { detail }));
+    dispatch(element, 'cable-ready:before-set-value', detail);
     element.value = value;
-    element.dispatchEvent(new CustomEvent('cable-ready:after-set-value', { detail }));
+    dispatch(element, 'cable-ready:after-set-value', detail);
   },
 
   // Attribute Mutations .....................................................................................
 
   setAttribute: detail => {
     const { element, name, value } = detail;
-    element.dispatchEvent(new CustomEvent('cable-ready:before-set-attribute', { detail }));
+    dispatch(element, 'cable-ready:before-set-attribute', detail);
     element.setAttribute(name, value);
-    element.dispatchEvent(new CustomEvent('cable-ready:after-set-attribute', { detail }));
+    dispatch(element, 'cable-ready:after-set-attribute', detail);
   },
 
   removeAttribute: detail => {
     const { element, name } = detail;
-    element.dispatchEvent(new CustomEvent('cable-ready:before-remove-attribute', { detail }));
+    dispatch(element, 'cable-ready:before-remove-attribute', detail);
     element.removeAttribute(name);
-    element.dispatchEvent(new CustomEvent('cable-ready:after-remove-attribute', { detail }));
+    dispatch(element, 'cable-ready:after-remove-attribute', detail);
   },
 
   // CSS Class Mutations .....................................................................................
 
   addCssClass: detail => {
     const { element, name } = detail;
-    element.dispatchEvent(new CustomEvent('cable-ready:before-add-css-class', { detail }));
+    dispatch(element, 'cable-ready:before-add-css-class', detail);
     element.classList.add(name);
-    element.dispatchEvent(new CustomEvent('cable-ready:after-add-css-class', { detail }));
+    dispatch(element, 'cable-ready:after-add-css-class', detail);
   },
 
   removeCssClass: detail => {
     const { element, name } = detail;
-    element.dispatchEvent(new CustomEvent('cable-ready:before-remove-css-class', { detail }));
+    dispatch(element, 'cable-ready:before-remove-css-class', detail);
     element.classList.remove(name);
-    element.dispatchEvent(new CustomEvent('cable-ready:after-remove-css-class', { detail }));
+    dispatch(element, 'cable-ready:after-remove-css-class', detail);
   },
 
   // Dataset Mutations .......................................................................................
 
   setDatasetProperty: detail => {
     const { element, name, value } = detail;
-    element.dispatchEvent(new CustomEvent('cable-ready:before-set-dataset-property', { detail }));
+    dispatch(element, 'cable-ready:before-set-dataset-property', detail);
     element.dataset[name] = value;
-    element.dispatchEvent(new CustomEvent('cable-ready:after-set-dataset-property', { detail }));
+    dispatch(element, 'cable-ready:after-set-dataset-property', detail);
   },
 };
 
