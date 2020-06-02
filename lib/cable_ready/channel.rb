b5cd25ae4ba6b527de0faee5a1e81c56569c0fe0
Hopsoft <natehop@gmail.com>
2020-06-01 21:51:17 -0600

    Version bump in prep for next release

--- lib/cable_ready/channel.rb
+++ lib/cable_ready/channel.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module CableReady
   class Channel
     attr_reader :name, :operations
--- lib/cable_ready/version.rb
+++ lib/cable_ready/version.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module CableReady
-  VERSION = "4.1.2"
+  VERSION = "4.2.0"
 end
