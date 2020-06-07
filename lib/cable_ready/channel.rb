51185fca9adb7f292ee37a1808eb676a6b248bc5
leastbad <hello@leastbad.com>
2020-06-06 23:56:20 -0400

    standardrb

--- lib/cable_ready/channel.rb
+++ lib/cable_ready/channel.rb
@@ -8,14 +8,14 @@ module CableReady
       @name = name
       @tools = tools
       @operations = stub
-      
+
       tools.each do |tool|
         define_singleton_method tool do |options = {}|
           add_operation tool, options
         end
       end
     end
-    
+
     def broadcast
       operations.select! { |_, list| list.present? }
       operations.deep_transform_keys! { |key| key.to_s.camelize(:lower) }
@@ -31,9 +31,8 @@ module CableReady
     end
 
     def stub
-      tools.inject({}) do |hash, tool|
+      tools.each_with_object({}) do |tool, hash|
         hash[tool] = []
-        hash
       end
     end
   end
