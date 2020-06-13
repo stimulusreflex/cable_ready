416b038098b04f55ce9753aa42440702fa5d8a54
leastbad <38150464+leastbad@users.noreply.github.com>
2020-06-13 09:27:26 -0400

    Customize operations via initializer (#47)
    * singleton channel with configurable tools via initializer
    
    * standardrb
    
    * uniq on tools
    
    * change tools to operations
    
    * allow people to define their own implementation
    
    * standardrb
    
    * available_operation
    
    * set_cookie client JS
    
    * farewell, stub
    
    * it's the purge
    
    * use %i[] and reintroduce reset method

--- lib/cable_ready/channel.rb
+++ lib/cable_ready/channel.rb
@@ -2,130 +2,15 @@
 
 module CableReady
   class Channel
-    attr_reader :identifier, :operations
+    attr_reader :identifier, :operations, :available_operations
 
-    # Example Operations Payload:
-    #
-    # {
-    #   # Cookies .....................................................................................................
-    #
-    #   set_cookie: [
-    #     {cookie: "example=value; path=/; expires=Sat, 07 Mar 2020 16:19:19 GMT"},
-    #     ...
-    #   ]
-    #
-    #   # DOM Events ..................................................................................................
-    #
-    #   dispatch_event: [{
-    #     name:     "string",
-    #     detail:   "object",
-    #     selector: "string",
-    #   }, ...],
-    #
-    #   # Element Mutations ...........................................................................................
-    #
-    #   morph: [{
-    #     selector:      "string",
-    #     html:          "string"
-    #     children_only:  true|false,
-    #     permanent_attribute_name: "string",
-    #     focus_selector: "string",
-    #   }, ...],
-    #
-    #   inner_html: [{
-    #     selector:      "string",
-    #     focus_selector: "string",
-    #     html:          "string"
-    #   }, ...],
-    #
-    #   outer_html: [{
-    #     selector:      "string",
-    #     focus_selector: "string",
-    #     html:          "string"
-    #   }, ...],
-    #
-    #   text_content: [{
-    #     selector: "string",
-    #     text:     "string"
-    #   }, ...]
-    #
-    #   insert_adjacent_html: [{
-    #     selector:      "string",
-    #     focus_selector: "string",
-    #     position:      "string",
-    #     html:          "string"
-    #   }, ...],
-    #
-    #   insert_adjacent_text: [{
-    #     selector: "string",
-    #     position: "string",
-    #     text:     "string"
-    #   }, ...],
-    #
-    #   remove: [{
-    #     selector:      "string",
-    #     focus_selector: "string,
-    #   }, ...],
-    #
-    #   set_property: [{
-    #     name:     "string",
-    #     selector: "string",
-    #     value:    "string"
-    #   }, ...],
-    #
-    #   set_value: [{
-    #     selector: "string",
-    #     value:    "string"
-    #   }, ...],
-    #
-    #   # Attribute Mutations .........................................................................................
-    #
-    #   set_attribute: [{
-    #     selector: "string",
-    #     name:     "string",
-    #     value:    "string"
-    #   }, ...],
-    #
-    #   remove_attribute: [{
-    #     selector: "string",
-    #     name:     "string"
-    #   }, ...],
-    #
-    #   # CSS Class Mutations .........................................................................................
-    #
-    #   add_css_class: [{
-    #     selector: "string",
-    #     name:     "string"
-    #   }, ...],
-    #
-    #   remove_css_class: [{
-    #     selector: "string",
-    #     name:     "string"
-    #   }, ...],
-    #
-    #   # Style Mutations ...........................................................................................
-    #
-    #   set_style: [{
-    #     selector: "string",
-    #     name:     "string",
-    #     value:    "string"
-    #   }, ...],
-    #
-    #   # Dataset Mutations ...........................................................................................
-    #
-    #   set_dataset_property: [{
-    #     selector: "string",
-    #     name:     "string",
-    #     value:    "string"
-    #   }, ...],
-    # }
-    def initialize(identifier)
+    def initialize(identifier, available_operations)
       @identifier = identifier
-      @operations = stub
-    end
-
-    def reset
-      @operations = stub
+      @available_operations = available_operations
+      reset
+      available_operations.each do |available_operation, implementation|
+        define_singleton_method available_operation, &implementation
+      end
     end
 
     def broadcast(clear)
@@ -142,105 +27,14 @@ module CableReady
       reset if clear
     end
 
-    def set_cookie(value)
-      add_operation(:set_cookie, {cookie: value})
-    end
-
-    def dispatch_event(options = {})
-      add_operation(:dispatch_event, options)
-    end
-
-    def morph(options = {})
-      add_operation(:morph, options)
-    end
-
-    def inner_html(options = {})
-      add_operation(:inner_html, options)
-    end
-
-    def outer_html(options = {})
-      add_operation(:outer_html, options)
-    end
-
-    def text_content(options = {})
-      add_operation(:text_content, options)
-    end
-
-    def insert_adjacent_html(options = {})
-      add_operation(:insert_adjacent_html, options)
-    end
-
-    def insert_adjacent_text(options = {})
-      add_operation(:insert_adjacent_text, options)
-    end
-
-    def remove(options = {})
-      add_operation(:remove, options)
-    end
-
-    def set_property(options = {})
-      add_operation(:set_property, options)
-    end
-
-    def set_value(options = {})
-      add_operation(:set_value, options)
-    end
-
-    def set_attribute(options = {})
-      add_operation(:set_attribute, options)
-    end
-
-    def remove_attribute(options = {})
-      add_operation(:remove_attribute, options)
-    end
-
-    def add_css_class(options = {})
-      add_operation(:add_css_class, options)
-    end
-
-    def remove_css_class(options = {})
-      add_operation(:remove_css_class, options)
-    end
-
-    def set_style(options = {})
-      add_operation(:set_style, options)
-    end
-
-    def set_styles(options = {})
-      add_operation(:set_styles, options)
-    end
-
-    def set_dataset_property(options = {})
-      add_operation(:set_dataset_property, options)
-    end
-
     private
 
     def add_operation(key, options)
-      operations[key] ||= []
       operations[key] << options
     end
 
-    def stub
-      {
-        add_css_class: [],
-        dispatch_event: [],
-        inner_html: [],
-        insert_adjacent_html: [],
-        insert_adjacent_text: [],
-        morph: [],
-        outer_html: [],
-        remove: [],
-        remove_attribute: [],
-        remove_css_class: [],
-        set_attribute: [],
-        set_cookie: [],
-        set_dataset_property: [],
-        set_style: [],
-        set_property: [],
-        set_value: [],
-        text_content: []
-      }
+    def reset
+      @operations = Hash.new { |hash, operation| hash[operation] = [] }
     end
   end
 end
--- lib/cable_ready/channels.rb
+++ lib/cable_ready/channels.rb
@@ -4,12 +4,45 @@ require_relative "channel"
 
 module CableReady
   class Channels
+    include Singleton
+    attr_accessor :operations
+
+    def self.configure
+      yield CableReady::Channels.instance if block_given?
+    end
+
     def initialize
       @channels = {}
+      @operations = {}
+      %i[
+        add_css_class
+        dispatch_event
+        inner_html
+        insert_adjacent_html
+        insert_adjacent_text
+        morph
+        outer_html
+        remove
+        remove_attribute
+        remove_css_class
+        set_attribute
+        set_cookie
+        set_dataset_property
+        set_property
+        set_style
+        set_styles
+        set_value
+      ].each do |operation|
+        add_operation operation
+      end
+    end
+
+    def add_operation(operation, &implementation)
+      @operations[operation] = implementation || ->(options = {}) { add_operation(operation, options) }
     end
 
     def [](identifier)
-      @channels[identifier] ||= CableReady::Channel.new(identifier)
+      @channels[identifier] ||= CableReady::Channel.new(identifier, operations)
     end
 
     def broadcast(*identifiers, clear: true)
