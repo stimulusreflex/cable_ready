c279e955be5ddc6a811629b310800ddcbf212818
Nate Hopkins <natehop@gmail.com>
2020-12-11 12:39:46 -0700

    Add support for sessionStorage and localStorage (#83)
    * Add support for sessionStorage and localStorage
    
    * Simplify type key
    
    * Complete rename of storageType to type

--- javascript/cable_ready.js
+++ javascript/cable_ready.js
@@ -119,6 +119,24 @@ const DOMOperations = {
     dispatch(document, 'cable-ready:after-push-state', config)
   },
 
+  // Storage .................................................................................................
+
+  storageSetItem: config => {
+    const { key, value, type } = config
+    dispatch(document, 'cable-ready:before-storage-set-item', config)
+    storage = type === 'session' ? sessionStorage : localStorage
+    storage.setItem(key, value)
+    dispatch(document, 'cable-ready:after-storage-set-item', config)
+  },
+
+  storageRemoveItem: config => {
+    const { key, type } = config
+    dispatch(document, 'cable-ready:before-storage-remove-item', config)
+    storage = type === 'session' ? sessionStorage : localStorage
+    storage.removeItem(key)
+    dispatch(document, 'cable-ready:after-storage-remove-item', config)
+  },
+
   // Notifications ...........................................................................................
 
   consoleLog: config => {
