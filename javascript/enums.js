a3114655252f54efd99d3768b658de3b4ca3e964
leastbad <38150464+leastbad@users.noreply.github.com>
2020-12-17 12:13:41 -0500

    pluggable before/after morph callbacks (#87)

---
+++ javascript/enums.js
@@ -0,0 +1,32 @@
+export const inputTags = {
+  INPUT: true,
+  TEXTAREA: true,
+  SELECT: true
+}
+
+export const mutableTags = {
+  INPUT: true,
+  TEXTAREA: true,
+  OPTION: true
+}
+
+export const textInputTypes = {
+  'datetime-local': true,
+  'select-multiple': true,
+  'select-one': true,
+  color: true,
+  date: true,
+  datetime: true,
+  email: true,
+  month: true,
+  number: true,
+  password: true,
+  range: true,
+  search: true,
+  tel: true,
+  text: true,
+  textarea: true,
+  time: true,
+  url: true,
+  week: true
+}
