e3ec7baf5a75ebc7eae46f77cf475ea634702cb5
Joshua LeBlanc <joshleblanc94@gmail.com>
2020-12-30 17:47:14 -0400

    don't replace currently functionality of []

--- lib/cable_ready/channels.rb
+++ lib/cable_ready/channels.rb
@@ -50,8 +50,11 @@ module CableReady
     end
 
     def [](identifier)
-      stream_name = stream_name_from(identifier)
-      @channels[stream_name] ||= CableReady::Channel.new(stream_name, operations)
+      @channels[identifier] ||= CableReady::Channel.new(identifier, operations)
+    end
+
+    def stream_to(identifier)
+      self[stream_name_from(identifier)]
     end
 
     def broadcast(*identifiers, clear: true)
