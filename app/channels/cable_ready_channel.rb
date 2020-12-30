d46b5473442d0aa0abd8aee0e83d931953c7bb3d
Joshua LeBlanc <joshleblanc94@gmail.com>
2020-12-30 18:08:48 -0400

    missing parenthesis

--- app/channels/cable_ready_channel.rb
+++ app/channels/cable_ready_channel.rb
@@ -2,7 +2,7 @@ class CableReadyChannel < ActionCable::Channel::Base
   include CableReady::StreamName
 
   def subscribed
-    verified_stream_name = verified_stream_name(params[:signed_stream_name]
+    verified_stream_name = verified_stream_name(params[:signed_stream_name])
     if verified_stream_name
       stream_from verified_stream_name
     else
