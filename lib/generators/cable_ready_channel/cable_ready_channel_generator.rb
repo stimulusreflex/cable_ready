cc6186d41c88e5e5b5421731065fd1ddb9e594f3
Julian Rubisch <julian@julianrubisch.at>
2021-01-06 22:15:44 +0100

    Generate stimulus controller from template

--- lib/generators/cable_ready_channel/cable_ready_channel_generator.rb
+++ lib/generators/cable_ready_channel/cable_ready_channel_generator.rb
@@ -4,14 +4,22 @@ class CableReadyChannelGenerator < Rails::Generators::NamedBase
   def create_channel
     generate "channel", name
 
-    identifier = ask("What is your stream identifier (what goes into stream_from)?")
+    if yes?("Are you streaming to a resource (are you using broadcast_to?")
+      resource = ask("Which resource are you streaming to (e.g. Post)?")
+      resource_name = resource.underscore
+      template "app/javascript/controllers/resource_controller.js", "app/javascript/controllers/#{resource_name}_controller.js"
 
-    prepend_to_file "app/javascript/channels/#{file_name}_channel.js", "import CableReady from 'cable_ready'\n"
-    inject_into_file "app/javascript/channels/#{file_name}_channel.js", after: "// Called when there's incoming data on the websocket for this channel\n" do <<-JS
+      gsub_file "app/channels/#{file_name}_channel.rb", %r{# stream_from.*\n}, "stream_to #{resource}.find(params[:id])\n"
+    else
+      identifier = ask("What is your stream identifier (what goes into stream_from)?")
+
+      prepend_to_file "app/javascript/channels/#{file_name}_channel.js", "import CableReady from 'cable_ready'\n"
+      inject_into_file "app/javascript/channels/#{file_name}_channel.js", after: "// Called when there's incoming data on the websocket for this channel\n" do <<-JS
     if (data.cableReady) CableReady.perform(data.operations)
-    JS
-    end
+      JS
+      end
 
-    gsub_file "app/channels/#{file_name}_channel.rb", %r{# stream_from.*\n}, "stream_from \"#{identifier}\"\n"
+      gsub_file "app/channels/#{file_name}_channel.rb", %r{# stream_from.*\n}, "stream_from \"#{identifier}\"\n"
+    end
   end
 end
