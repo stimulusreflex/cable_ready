565976b4e0a4333897377fc99c4a18862839f8d7
Julian Rubisch <julian@julianrubisch.at>
2021-01-06 19:28:25 +0100

    Inject stream identifier

--- lib/generators/cable_ready_channel/cable_ready_channel_generator.rb
+++ lib/generators/cable_ready_channel/cable_ready_channel_generator.rb
@@ -4,10 +4,14 @@ class CableReadyChannelGenerator < Rails::Generators::NamedBase
   def create_channel
     generate "channel", name
 
+    identifier = ask("What is your stream identifier (what goes into stream_from)?")
+
     prepend_to_file "app/javascript/channels/#{file_name}_channel.js", "import CableReady from 'cable_ready'\n"
     inject_into_file "app/javascript/channels/#{file_name}_channel.js", after: "// Called when there's incoming data on the websocket for this channel\n" do <<-JS
     if (data.cableReady) CableReady.perform(data.operations)
     JS
     end
+
+    gsub_file "app/channels/#{file_name}_channel.rb", %r{# stream_from.*\n}, "stream_from \"#{identifier}\"\n"
   end
 end
