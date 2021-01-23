d358306ab546bf4e6357627c41d40402ee5f0bc3
Nate Hopkins <natehop@gmail.com>
2021-01-23 06:50:22 -0700

    Global config, simplify threading, custom operations (#96)
    * Global config, simplify threading, custom operations
    
    * Remove unused artifact
    
    * Remove redundant require
    
    * GitBook: [master] one page modified
    
    * Support chaining and rip deprecations out
    
    * GitBook: [master] one page modified
    
    * multiple selector element operations (#92)
    
    * `select_all` option added to all operations which support a `selector`
    
    * `cancel` option added to almost all operations, intended to be set in a `before-` event callback
    
    * substantial reorganization of `cable_ready.js` to match the order in the docs
    
    * Bump nokogiri from 1.10.10 to 1.11.1
    
    Bumps [nokogiri](https://github.com/sparklemotion/nokogiri) from 1.10.10 to 1.11.1.
    - [Release notes](https://github.com/sparklemotion/nokogiri/releases)
    - [Changelog](https://github.com/sparklemotion/nokogiri/blob/master/CHANGELOG.md)
    - [Commits](https://github.com/sparklemotion/nokogiri/compare/v1.10.10...v1.11.1)
    
    Signed-off-by: dependabot[bot] <support@github.com>
    
    * Add `append`, `prepend` and `replace` operations (#90)
    
    * Disable chaining after broadcast
    
    Co-authored-by: leastbad <hello@leastbad.com>
    Co-authored-by: leastbad <38150464+leastbad@users.noreply.github.com>
    Co-authored-by: dependabot[bot] <49699333+dependabot[bot]@users.noreply.github.com>
    Co-authored-by: Marco Roth <marco.roth@intergga.ch>

---
+++ lib/cable_ready/config.rb
@@ -0,0 +1,63 @@
+# frozen_string_literal: true
+
+require "monitor"
+require "observer"
+require "singleton"
+
+module CableReady
+  # This class is a process level singleton shared by all threads: CableReady::Config.instance
+  class Config
+    include MonitorMixin
+    include Observable
+    include Singleton
+
+    def initialize
+      super
+      @operation_names = Set.new(default_operation_names)
+    end
+
+    def operation_names
+      @operation_names.to_a
+    end
+
+    def add_operation_name(name)
+      synchronize do
+        @operation_names << name.to_sym
+        notify_observers name.to_sym
+      end
+    end
+
+    def default_operation_names
+      Set.new(%i[
+        add_css_class
+        append
+        clear_storage
+        console_log
+        dispatch_event
+        inner_html
+        insert_adjacent_html
+        insert_adjacent_text
+        morph
+        notification
+        outer_html
+        prepend
+        push_state
+        remove
+        remove_attribute
+        remove_css_class
+        remove_storage_item
+        replace
+        set_attribute
+        set_cookie
+        set_dataset_property
+        set_focus
+        set_property
+        set_storage_item
+        set_style
+        set_styles
+        set_value
+        text_content
+      ]).freeze
+    end
+  end
+end
