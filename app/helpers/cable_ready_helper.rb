2fdbf90aa827d0109ea1db42299c62c08f67ef3b
leastbad <hello@leastbad.com>
2021-01-27 19:03:10 -0500

    support multiple identifiers

--- app/helpers/cable_ready_helper.rb
+++ app/helpers/cable_ready_helper.rb
@@ -1,11 +1,16 @@
 # frozen_string_literal: true
 
 module CableReadyHelper
-  def stream_from(key)
-    if key.is_a? String
-      tag.div data: {controller: "stream-from", stream_from_identifier_value: key}
-    else
-      tag.div data: {controller: "stream-from", stream_from_sgid_value: key.to_sgid(expires_in: nil).to_s}
+  def stream_from(*keys)
+    tags = []
+    keys.each do |key|
+      if [String, Symbol].include? key.class
+        tags << tag.div(data: {controller: "stream-from", stream_from_identifier_value: key})
+      end
+      if key.class < ActiveRecord::Base
+        tags << tag.div(data: {controller: "stream-from", stream_from_sgid_value: key.to_sgid(expires_in: nil).to_s})
+      end
     end
+    tags.join("\n").html_safe
   end
 end
