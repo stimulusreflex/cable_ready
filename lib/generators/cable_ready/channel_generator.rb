f6290d480fe0875475fc36f1a48c40b5e2484eeb
Marco Roth <marco.roth@intergga.ch>
2021-02-01 21:45:11 +0100

    change channel generator to make more use of --stream-for and --stream-from

--- lib/generators/cable_ready/channel_generator.rb
+++ lib/generators/cable_ready/channel_generator.rb
@@ -3,26 +3,23 @@
 class CableReady::ChannelGenerator < Rails::Generators::NamedBase
   source_root File.expand_path("templates", __dir__)
 
+  class_option :stream_from, type: :string
+  class_option :stream_for, type: :string
   class_option :stimulus, type: :boolean
-  class_option :broadcast_to, type: :boolean
-  class_option :resource, type: :string
-  class_option :identifier, type: :string
+
+  def check_options
+    raise "Can't specify --stream-from and --stream-for at the same time" if options.key?(:stream_from) && options.key?(:stream_for)
+  end
 
   def create_channel
     generate "channel", file_name
+  end
 
+  def enhance_channels
     if using_broadcast_to?
-      resource = ask_for_resource
-      resource_name = resource.underscore
-
-      gsub_file "app/channels/#{file_name}_channel.rb", %r{# stream_from.*\n}, "stream_for #{resource}.find(params[:id])\n"
-
-      if using_stimulus?
-        template "app/javascript/controllers/%file_name%_controller.js"
-      end
+      gsub_file "app/channels/#{file_name}_channel.rb", /# stream_from.*\n/, "stream_for #{resource}.find(params[:id])\n"
+      template "app/javascript/controllers/%file_name%_controller.js" if using_stimulus?
     else
-      identifier = ask_for_identifier
-
       prepend_to_file "app/javascript/channels/#{file_name}_channel.js", "import CableReady from 'cable_ready'\n"
       inject_into_file "app/javascript/channels/#{file_name}_channel.js", after: "// Called when there's incoming data on the websocket for this channel\n" do
         <<-JS
@@ -30,16 +27,18 @@ class CableReady::ChannelGenerator < Rails::Generators::NamedBase
         JS
       end
 
-      gsub_file "app/channels/#{file_name}_channel.rb", %r{# stream_from.*\n}, "stream_from \"#{identifier}\"\n"
+      gsub_file "app/channels/#{file_name}_channel.rb", /# stream_from.*\n/, "stream_from \"#{identifier}\"\n"
     end
   end
 
   private
 
+  def option_given?
+    options.key?(:stream_from) || options.key?(:stream_for)
+  end
+
   def using_broadcast_to?
-    @using_broadcast_to ||= options.fetch(:broadcast_to) {
-      yes?("Are you streaming to a resource using broadcast_to? (y/N)")
-    }
+    @using_broadcast_to ||= option_given? ? options.key?(:stream_for) : yes?("Are you streaming to a resource using broadcast_to? (y/N)")
   end
 
   def using_stimulus?
@@ -48,15 +47,25 @@ class CableReady::ChannelGenerator < Rails::Generators::NamedBase
     }
   end
 
-  def ask_for_resource
-    @resource ||= options.fetch(:resource).camelize {
-      ask("Which resource are you streaming to?", default: class_name).camelize
+  def resource
+    return @resource if @resource
+
+    stream_for = options.fetch(:stream_for) {
+      ask("Which resource are you streaming for?", default: class_name)
     }
+
+    stream_for = file_name if stream_for == "stream_for"
+    @resource = stream_for.camelize
   end
 
-  def ask_for_identifier
-    @identifier ||= options.fetch(:identifier).underscore {
-      ask("What is the stream identifier that goes into stream_from?", default: file_name).underscore
+  def identifier
+    return @identifier if @identifier
+
+    stream_from = options.fetch(:stream_from) {
+      ask("What is the stream identifier that goes into stream_from?", default: file_name)
     }
+
+    stream_from = file_name if stream_from == "stream_from"
+    @identifier = stream_from.underscore
   end
 end
--- test/lib/generators/cable_ready/channel_generator_test.rb
+++ test/lib/generators/cable_ready/channel_generator_test.rb
@@ -17,23 +17,23 @@ class CableReady::ChannelGeneratorTest < Rails::Generators::TestCase
   end
 
   test "should generate channel with the same resource name and stimulus controller" do
-    run_generator ["user", "--broadcast-to", "--resource=user", "--stimulus"]
+    run_generator ["user", "--stream-for=user", "--stimulus"]
 
     assert_file "app/channels/user_channel.rb" do |content|
-      assert_match /class\ UserChannel\ \</, content
-      assert_match /stream_for\ User\.find\(params\[\:id\]\)/, content
+      assert_match(/class\ UserChannel\ </, content)
+      assert_match(/stream_for\ User\.find\(params\[:id\]\)/, content)
     end
 
     assert_file "app/javascript/channels/user_channel.js", /"UserChannel"/
-    assert_file "app/javascript/controllers/user_controller.js", /\'UserChannel\'/
+    assert_file "app/javascript/controllers/user_controller.js", /'UserChannel'/
   end
 
   test "should generate channel with different resource name" do
-    run_generator ["my_name", "--broadcast-to", "--resource=under_scored_resource_name", "--no-stimulus"]
+    run_generator ["my_name", "--stream-for=under_scored_resource_name", "--no-stimulus"]
 
     assert_file "app/channels/my_name_channel.rb" do |content|
-      assert_match /class\ MyNameChannel\ \</, content
-      assert_match /stream_for\ UnderScoredResourceName\.find\(params\[\:id\]\)/, content
+      assert_match(/class\ MyNameChannel\ </, content)
+      assert_match(/stream_for\ UnderScoredResourceName\.find\(params\[:id\]\)/, content)
     end
 
     assert_file "app/javascript/channels/my_name_channel.js", /"MyNameChannel"/
@@ -41,7 +41,7 @@ class CableReady::ChannelGeneratorTest < Rails::Generators::TestCase
   end
 
   test "should not generate stimulus controller if not requested" do
-    run_generator ["comment", "--broadcast-to", "--resource=comment", "--no-stimulus"]
+    run_generator ["comment", "--stream-for=comment", "--no-stimulus"]
 
     assert_file "app/channels/comment_channel.rb"
     assert_file "app/javascript/channels/comment_channel.js"
@@ -49,32 +49,65 @@ class CableReady::ChannelGeneratorTest < Rails::Generators::TestCase
   end
 
   test "should run the generator when streaming from identifier" do
-    run_generator ["page", "--no-broadcast-to", "--identifier=page"]
+    run_generator ["page", "--stream-from=page"]
 
     assert_file "app/channels/page_channel.rb" do |content|
-      assert_match /PageChannel/, content
-      assert_match /stream_from\ \"page\"/, content
+      assert_match(/PageChannel/, content)
+      assert_match(/stream_from\ "page"/, content)
     end
 
     assert_file "app/javascript/channels/page_channel.js" do |content|
-      assert_match /"PageChannel"/, content
-      assert_match /import\ CableReady/, content
-      assert_match /if\ \(data\.cableReady\)\ CableReady\.perform\(data\.operations\)/, content
+      assert_match(/"PageChannel"/, content)
+      assert_match(/import\ CableReady/, content)
+      assert_match(/if\ \(data\.cableReady\)\ CableReady\.perform\(data\.operations\)/, content)
     end
   end
 
   test "should run the generator when streaming without resource and different identifier" do
-    run_generator ["my_page", "--no-broadcast-to", "--identifier=ThisIsMyPage"]
+    run_generator ["my_page", "--stream-from=ThisIsMyPage"]
 
     assert_file "app/channels/my_page_channel.rb" do |content|
-      assert_match /MyPageChannel/, content
-      assert_match /stream_from\ \"this_is_my_page\"/, content
+      assert_match(/MyPageChannel/, content)
+      assert_match(/stream_from\ "this_is_my_page"/, content)
     end
 
     assert_file "app/javascript/channels/my_page_channel.js" do |content|
-      assert_match /"MyPageChannel"/, content
-      assert_match /import\ CableReady/, content
-      assert_match /if\ \(data\.cableReady\)\ CableReady\.perform\(data\.operations\)/, content
+      assert_match(/"MyPageChannel"/, content)
+      assert_match(/import\ CableReady/, content)
+      assert_match(/if\ \(data\.cableReady\)\ CableReady\.perform\(data\.operations\)/, content)
+    end
+  end
+
+  test "should run the generator and use the NAME for --stream-from if nothing passed" do
+    run_generator ["house", "--stream-from"]
+
+    assert_file "app/channels/house_channel.rb" do |content|
+      assert_match(/HouseChannel/, content)
+      assert_match(/stream_from\ "house"/, content)
+    end
+
+    assert_file "app/javascript/channels/house_channel.js" do |content|
+      assert_match(/"HouseChannel"/, content)
+      assert_match(/import\ CableReady/, content)
+      assert_match(/if\ \(data\.cableReady\)\ CableReady\.perform\(data\.operations\)/, content)
+    end
+  end
+
+  test "should run the generator and use the NAME for --stream-for if nothing passed" do
+    run_generator ["option", "--stream-for", "--stimulus"]
+
+    assert_file "app/channels/option_channel.rb" do |content|
+      assert_match(/class\ OptionChannel\ </, content)
+      assert_match(/stream_for\ Option\.find\(params\[:id\]\)/, content)
+    end
+
+    assert_file "app/javascript/channels/option_channel.js", /"OptionChannel"/
+    assert_file "app/javascript/controllers/option_controller.js", /'OptionChannel'/
+  end
+
+  test "should run not generate anything if passed stream_from and stream_for" do
+    assert_raises "Can't specify --stream-from and --stream-for at the same time" do
+      run_generator ["error", "--stream-from=1", "--stream-for=2"]
     end
   end
 
@@ -82,24 +115,23 @@ class CableReady::ChannelGeneratorTest < Rails::Generators::TestCase
 
   test "should generate channel with the same resource name and stimulus controller (without options)" do
     CableReady::ChannelGenerator.any_instance.stubs(:using_broadcast_to?).returns(true)
-    CableReady::ChannelGenerator.any_instance.stubs(:ask_for_resource).returns("Post")
+    CableReady::ChannelGenerator.any_instance.stubs(:resource).returns("Post")
     CableReady::ChannelGenerator.any_instance.stubs(:using_stimulus?).returns(true)
 
     run_generator ["post"]
 
     assert_file "app/channels/post_channel.rb" do |content|
-      assert_match /class\ PostChannel\ \</, content
-      assert_match /stream_for\ Post\.find\(params\[\:id\]\)/, content
+      assert_match(/class\ PostChannel\ </, content)
+      assert_match(/stream_for\ Post\.find\(params\[:id\]\)/, content)
     end
 
     assert_file "app/javascript/channels/post_channel.js", /PostChannel/
-    assert_file "app/javascript/controllers/post_controller.js", /\'PostChannel\'/
+    assert_file "app/javascript/controllers/post_controller.js", /'PostChannel'/
   end
-
 
   test "should not generate stimulus controller if not requested (without options)" do
     CableReady::ChannelGenerator.any_instance.stubs(:using_broadcast_to?).returns(true)
-    CableReady::ChannelGenerator.any_instance.stubs(:ask_for_resource).returns("Admin")
+    CableReady::ChannelGenerator.any_instance.stubs(:resource).returns("Admin")
     CableReady::ChannelGenerator.any_instance.stubs(:using_stimulus?).returns(false)
 
     run_generator ["admin"]
@@ -111,13 +143,13 @@ class CableReady::ChannelGeneratorTest < Rails::Generators::TestCase
 
   test "should run the generator when streaming from identifier (without options)" do
     CableReady::ChannelGenerator.any_instance.stubs(:using_broadcast_to?).returns(false)
-    CableReady::ChannelGenerator.any_instance.stubs(:ask_for_identifier).returns("index_identifier")
+    CableReady::ChannelGenerator.any_instance.stubs(:identifier).returns("index_identifier")
 
     run_generator ["index"]
 
     assert_file "app/channels/index_channel.rb" do |content|
-      assert_match /IndexChannel/, content
-      assert_match /stream_from\ \"index_identifier\"/, content
+      assert_match(/IndexChannel/, content)
+      assert_match(/stream_from\ "index_identifier"/, content)
     end
 
     assert_file "app/javascript/channels/index_channel.js", /"IndexChannel"/
