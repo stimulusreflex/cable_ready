4ecaa1304ca67509f98bd5bdaae943a1609a0d07
leastbad <hello@leastbad.com>
2021-02-07 00:22:46 -0500

    cable_car aka 'Ajax mode'

---
+++ lib/cable_ready/cable_car.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require "thread/local"
+
+module CableReady
+  class CableCar
+    extend Thread::Local
+    attr_reader :enqueued_operations
+
+    def initialize
+      reset
+      CableReady.config.operation_names.each { |name| add_operation_method name }
+
+      config_observer = self
+      CableReady.config.add_observer config_observer, :add_operation_method
+      ObjectSpace.define_finalizer self, -> { CableReady.config.delete_observer config_observer }
+    end
+
+    def to_json(clear: true)
+      json = {"cableReady" => true, "operations" => transmitable_operations}.to_json
+      reset if clear
+      json
+    end
+
+    def add_operation_method(name)
+      return if respond_to?(name)
+      singleton_class.public_send :define_method, name, ->(options = {}) {
+        enqueued_operations[name.to_s] << options.stringify_keys
+        self # supports operation chaining
+      }
+    end
+
+    private
+
+    def reset
+      @enqueued_operations = Hash.new { |hash, key| hash[key] = [] }
+    end
+
+    def transmitable_operations
+      enqueued_operations
+        .select { |_, list| list.present? }
+        .deep_transform_keys! { |key| key.to_s.camelize(:lower) }
+    end
+  end
+end
