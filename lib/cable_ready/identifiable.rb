450ff6161c58ecc784de278de2f2d10716d0d42b
Marco Roth <marco.roth@intergga.ch>
2021-04-07 01:19:54 +0200

    Tests for `CableReady::Identifiable` and `CableReady::OperationBuilder` (#4)

--- lib/cable_ready/identifiable.rb
+++ lib/cable_ready/identifiable.rb
@@ -1,16 +1,22 @@
 # frozen_string_literal: true
 
+require "active_record"
+require "action_view"
+
 module CableReady
   module Identifiable
-    def dom_id(record, prefix = nil, hash: "#")
+    def dom_id(record, prefix = nil)
+      prefix = prefix.to_s.strip if prefix
+
       id = if record.is_a?(ActiveRecord::Relation)
         [prefix, record.model_name.plural].compact.join("_")
       elsif record.is_a?(ActiveRecord::Base)
-        ActionView::RecordIdentifier.dom_id(record, prefix).to_s
+        ActionView::RecordIdentifier.dom_id(record, prefix)
       else
-        [prefix, record.to_s].compact.join("_")
+        [prefix, record.to_s.strip].compact.join("_")
       end
-      (hash + id).squeeze("#")
+
+      "##{id}".squeeze("#").strip
     end
   end
 end
--- lib/cable_ready/operation_builder.rb
+++ lib/cable_ready/operation_builder.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require "active_record"
+
 require_relative "identifiable"
 
 module CableReady
