49a1b6389736d85eb0f78845fedaf54ec6a8e6d7
Rafe Rosen <rafe@existentialmutt.com>
2021-06-13 18:22:14 -0400

    Add system test generator

--- lib/generators/cable_ready/scaffold/scaffold_generator.rb
+++ lib/generators/cable_ready/scaffold/scaffold_generator.rb
@@ -12,15 +12,18 @@ class CableReady::ScaffoldGenerator < Rails::Generators::ResourceGenerator
 
   check_class_collision suffix: "Controller"
 
+  class_option :skip_system_tests, type: :boolean, default: false, desc: "Skip system test files"
+
   def create_controller_files
-    template "controller.rb", File.join("app/controllers", controller_class_path, "#{controller_file_name}_controller.rb")
+    template "controller.rb",
+      File.join("app/controllers", controller_class_path, "#{controller_file_name}_controller.rb")
   end
 
   def create_view_root
     empty_directory File.join("app/views", controller_file_path)
   end
 
-  # TODO ERB only for now, add alternate template engine support
+  # TODO: ERB only for now, add alternate template engine support
   def copy_view_files
     available_views.each do |view|
       target_view = if view == "_resource"
@@ -44,7 +47,7 @@ class CableReady::ScaffoldGenerator < Rails::Generators::ResourceGenerator
       end
     else
       main_folder = File.join("app", "assets", "javascripts")
-      # TODO automate?
+      # TODO: automate?
       say "Please be sure Stimulus is installed. See https://github.com/hotwired/stimulus-rails"
     end
 
@@ -59,15 +62,18 @@ class CableReady::ScaffoldGenerator < Rails::Generators::ResourceGenerator
     lines = File.open(filepath, "r") { |f| f.readlines }
 
     unless lines.find { |line| line.start_with?("import Rails") }
-      matches = lines.select { |line| line =~ /\A(require|import)/ }
-      lines.insert lines.index(matches.last).to_i + 1, "import Rails from '@rails/ujs'\nRails.start()\n"
-      File.open(filepath, "w") { |f| f.write lines.join }
-
       say "Adding @rails/ujs via yarn"
       system "bin/yarn add @rails/ujs"
+      append_file filepath, "\nimport Rails from '@rails/ujs'\nRails.start()\n"
     end
 
     template "cable_car_controller.js", File.join(main_folder, "controllers", "cable_car_controller.js")
+  end
+
+  def create_test_files
+    unless options[:skip_system_tests]
+      template "system_test.rb", File.join("test/system", class_path, "#{file_name.pluralize}_test.rb")
+    end
   end
 
   private
@@ -86,5 +92,37 @@ class CableReady::ScaffoldGenerator < Rails::Generators::ResourceGenerator
   def attachments?(name)
     attribute = attributes.find { |attr| attr.name == name }
     attribute&.attachments?
+  end
+
+  # For system tests
+  def attributes_hash
+    return {} if attributes_names.empty?
+
+    attributes_names.map do |name|
+      if %w[password password_confirmation].include?(name) && attributes.any?(&:password_digest?)
+        [name.to_s, "'secret'"]
+      elsif !virtual?(name)
+        [name.to_s, "@#{singular_table_name}.#{name}"]
+      end
+    end.compact.sort.to_h
+  end
+
+  def boolean?(name)
+    attribute = attributes.find { |attr| attr.name == name }
+    attribute&.type == :boolean
+  end
+
+  def virtual?(name)
+    attribute = attributes.find { |attr| attr.name == name }
+    attribute&.virtual?
+  end
+
+  def fixture_name
+    @fixture_name ||=
+      if mountable_engine?
+        (namespace_dirs + [table_name]).join("_")
+      else
+        table_name
+      end
   end
 end
