b3fc429d775d039af4189cc36cc87fbdf949ea37
leastbad <hello@leastbad.com>
2021-08-14 17:23:56 -0400

    progress

--- lib/cable_ready/broadcaster.rb
+++ lib/cable_ready/broadcaster.rb
@@ -5,12 +5,40 @@ module CableReady
     include Identifiable
     extend ::ActiveSupport::Concern
 
+    included do |base|
+      if base < ActiveRecord::Base
+        after_commit :broadcast_self
+      end
+    end
+
+    class_methods do |base|
+      if base < ActiveRecord::Base
+        def has_many(*args, &block)
+          options = args.extract_options!
+          options[:extend] = Array(options[:extend]).push(ClassMethods)
+          super(*args, **options, &block)
+        end
+      end
+    end
+
     def cable_ready
       CableReady::Channels.instance
     end
 
     def cable_car
       CableReady::CableCar.instance
+    end
+
+    private
+
+    def broadcast_self
+      ActionCable.server.broadcast("#{self.class.name.underscore}_#{id}", {})
+    end
+
+    module ClassMethods
+      def broadcast_collection(id, collection_name)
+        ActionCable.server.broadcast("#{name.underscore}_#{id}_#{collection_name}", {})
+      end
     end
   end
 end
