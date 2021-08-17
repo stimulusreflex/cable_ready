451854f4006b740623c269e238906c376a8d6dd4
leastbad <hello@leastbad.com>
2021-08-17 13:51:41 -0400

    progress!

--- lib/cable_ready/broadcaster.rb
+++ lib/cable_ready/broadcaster.rb
@@ -7,25 +7,17 @@ module CableReady
 
     included do |base|
       if base < ActiveRecord::Base
-        class_attribute :broadcast_resource_enabled
-        after_commit :broadcast_resource_commits, if: -> { self.broadcast_resource_enabled }
+        include ExtendHasMany
+        class_attribute :broadcast_self_enabled, :broadcast_collections_enabled
+        after_commit :broadcast_self, if: -> { broadcast_self_enabled }
+        after_commit :broadcast_collections, if: -> { broadcast_collections_enabled }
 
-        def self.has_many(name, scope = nil, **options, &extension)
-          broadcast = options.delete(:broadcast).present?
-          result = super
-          broadcast_association(name) if broadcast
-          result
+        def self.enable_broadcast_self
+          self.broadcast_self_enabled = true
         end
 
-        def self.broadcast_resource
-          self.broadcast_resource_enabled = true
-        end
-
-        private
-
-        def self.broadcast_association(name)
-          # name is the name of the association, so that's one down...
-          # ActionCable.server.broadcast(self.to_gid, {})
+        def self.enable_broadcast_collections
+          self.broadcast_collections_enabled = true
         end
       end
     end
@@ -40,8 +32,54 @@ module CableReady
 
     private
 
-    def broadcast_resource_commits
-      ActionCable.server.broadcast(self.to_gid, {})
+    def broadcast_self
+      ActionCable.server.broadcast(to_global_id, {})
+    end
+
+    def broadcast_collections
+      self.class.registered_collections.each do |registered_collection|
+        # klass, id, collection, resource = registered_collection
+        klass, _, collection = registered_collection
+        puts
+        puts "*******************"
+        puts registered_collection.inspect
+        puts "************"
+        puts
+        klass.broadcast_collection(self, collection)
+      end
+    end
+
+    module ClassMethods
+      def broadcast_collection(resource, collection_name)
+        identifier = CableReady.signed_stream_verifier.generate(resource.to_global_id.to_s + ":" + collection_name.to_s)
+        ActionCable.server.broadcast(identifier, {})
+      end
+
+      # def broadcast(resource, collection_name)
+      def broadcast(collection_name)
+        reflection = reflect_on_association(collection_name)
+        # reflection.klass.register_collection(self, reflection.foreign_key, collection_name, resource)
+        reflection.klass.register_collection(self, reflection.foreign_key, collection_name)
+      end
+
+      def has_many(name, scope = nil, **options, &extension)
+        broadcast = options.delete(:broadcast).present?
+        result = super
+        # broadcast(self, name) if broadcast
+        broadcast(name) if broadcast
+        result
+      end
+
+      # def register_collection(klass, id, collection, resource)
+      def register_collection(klass, id, collection)
+        @collections ||= []
+        # @collections << [klass, id, collection, resource]
+        @collections << [klass, id, collection]
+      end
+
+      def registered_collections
+        @collections || []
+      end
     end
   end
 end
---
+++ lib/cable_ready/extend_has_many.rb
@@ -0,0 +1,11 @@
+module ExtendHasMany
+  extend ::ActiveSupport::Concern
+
+  class_methods do
+    def has_many(*args, &block)
+      options = args.extract_options!
+      options[:extend] = Array(options[:extend]).push(ClassMethods)
+      super(*args, **options, &block)
+    end
+  end
+end
