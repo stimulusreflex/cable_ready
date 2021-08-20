dff8cac98ae1c638de2b63ecb773a746bc2d0807
leastbad <hello@leastbad.com>
2021-08-20 06:23:21 -0400

    has_many broadcast: if and on working

--- lib/cable_ready/broadcaster.rb
+++ lib/cable_ready/broadcaster.rb
@@ -31,14 +31,22 @@ module CableReady
 
     private
 
+    def current_callback
+      return :create if instance_variable_defined?("@_trigger_create_callback")
+      return :update if instance_variable_defined?("@_trigger_update_callback")
+      return :destroy if instance_variable_defined?("@_trigger_destroy_callback")
+    end
+
     def broadcast_self
       ActionCable.server.broadcast(to_global_id, {})
     end
 
     def broadcast_collections
-      self.class.registered_collections.each do |collection|
+      self.class.registered_collections
+        .select { |c| c[:options][:on].include?(current_callback) }
+        .each do |collection|
         resource = find_resource_for_broadcast(collection)
-        collection[:klass].broadcast_collection(resource, collection[:name])
+        collection[:klass].broadcast_collection(resource, collection[:name]) if collection[:options][:if].call(resource)
       end
     end
 
@@ -49,12 +57,29 @@ module CableReady
     end
 
     module ClassMethods
+      def has_many(name, scope = nil, **options, &extension)
+        option = options.delete(:broadcast)
+        broadcast = option.present?
+        result = super
+        broadcast(name, option) if broadcast
+        result
+      end
+
+      def register_collection(collection)
+        @collections ||= []
+        @collections << collection
+      end
+
+      def registered_collections
+        @collections || []
+      end
+
       def broadcast_collection(resource, name)
         identifier = resource.to_global_id.to_s + ":" + name.to_s
         ActionCable.server.broadcast(identifier, {})
       end
 
-      def broadcast(name)
+      def broadcast(name, option)
         reflection = reflect_on_association(name)
 
         inverse_of = reflection.inverse_of&.name&.to_s
@@ -67,29 +92,37 @@ module CableReady
 
         raise ArgumentError, "Could not find inverse_of for #{name}" unless inverse_of
 
-        reflection.klass.register_collection({ 
+        options = {
+          on: [:create, :update, :destroy],
+          if: ->(resource) { true }
+        }
+
+        case option
+        when TrueClass
+          # proceed!
+        when FalseClass
+          options[:on] = []
+        when Array
+          options[:on] = option
+        when Symbol
+          options[:on] = [option]
+        when Hash
+          option[:on] = Array(option[:on]) if option[:on]
+          options = options.merge!(option)
+        when Proc
+          options[:if] = option
+        else
+          raise ArgumentError, "Invalid broadcast option #{option}"
+        end
+
+        reflection.klass.register_collection({
           klass: self,
           foreign_key: reflection.foreign_key,
           name: name,
           inverse_association: inverse_of,
-          through_association: through_association
+          through_association: through_association,
+          options: options
         })
-      end
-
-      def has_many(name, scope = nil, **options, &extension)
-        broadcast = options.delete(:broadcast).present?
-        result = super
-        broadcast(name) if broadcast
-        result
-      end
-
-      def register_collection(collection)
-        @collections ||= []
-        @collections << collection
-      end
-
-      def registered_collections
-        @collections || []
       end
     end
   end
