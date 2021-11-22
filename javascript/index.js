8da9d7cf407b69df3df548f2a7caa67620e6fdb3
leastbad <38150464+leastbad@users.noreply.github.com>
2021-11-22 04:24:45 -0500

    Move sanity checking to client-server interaction (#162)
    * pass CR version with broadcast
    * broadcast and broadcast_to now return client recipient counts

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,4 @@
+import { version } from '../package.json'
 import * as MorphCallbacks from './morph_callbacks'
 import { shouldMorphCallbacks, didMorphCallbacks } from './morph_callbacks'
 import * as Utils from './utils'
@@ -24,6 +25,7 @@ export default {
   consumer,
   addOperation,
   addOperations,
+  version,
   get DOMOperations () {
     console.warn(
       'DEPRECATED: Please use `CableReady.operations` instead of `CableReady.DOMOperations`'
