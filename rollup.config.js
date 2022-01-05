22e2eea814143fa47b9f371ea063a82353ade55d
Marco Roth <marco.roth@intergga.ch>
2022-01-05 11:07:18 +0100

    also lint `rollup.config.js`

--- rollup.config.js
+++ rollup.config.js
@@ -1,35 +1,31 @@
-import resolve from "@rollup/plugin-node-resolve"
-import commonjs from "@rollup/plugin-commonjs";
-import json from "@rollup/plugin-json";
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs'
+import json from '@rollup/plugin-json'
 
-const basePlugins = [
-  resolve(),
-  commonjs(),
-  json()
-]
+const basePlugins = [resolve(), commonjs(), json()]
 
 export default [
   {
-    external: ["morphdom"],
-    input: "javascript/index.js",
+    external: ['morphdom'],
+    input: 'javascript/index.js',
     output: [
       {
-        name: "CableReady",
-        file: "dist/cable_ready.umd.js",
-        format: "umd",
+        name: 'CableReady',
+        file: 'dist/cable_ready.umd.js',
+        format: 'umd',
         sourcemap: true,
-        exports: "named",
-        globals: {morphdom: "morphdom"}, // UMD build wants a global...annoying.
+        exports: 'named',
+        globals: { morphdom: 'morphdom' } // UMD build wants a global...annoying.
       },
       {
-        file: "dist/cable_ready.module.js",
-        format: "es",
-        sourcemap: true,
+        file: 'dist/cable_ready.module.js',
+        format: 'es',
+        sourcemap: true
       }
     ],
     plugins: basePlugins,
     watch: {
-      include: "javascript/**"
+      include: 'javascript/**'
     }
-  },
+  }
 ]
