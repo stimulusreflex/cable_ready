2bc4e9d207305062f77b42aed549a2081adfae6d
Marco Roth <marco.roth@intergga.ch>
2022-03-17 11:15:29 +0100

    add `cable_ready.importmap` initializer

--- lib/cable_ready/engine.rb
+++ lib/cable_ready/engine.rb
@@ -20,5 +20,12 @@ module CableReady
         Rails.application.config.assets.precompile += %w[cable_ready.js cable_ready.min.js cable_ready.min.js.map]
       end
     end
+
+    initializer "cable_ready.importmap", before: "importmap" do |app|
+      if Rails.application.config.respond_to?(:importmap)
+        Rails.application.config.importmap.paths << Engine.root.join("lib/cable_ready/importmap.rb")
+        Rails.application.config.importmap.cache_sweepers << Engine.root.join("app/assets/javascripts")
+      end
+    end
   end
 end
---
+++ lib/cable_ready/importmap.rb
@@ -0,0 +1,1 @@
+pin "morphdom", to: "https://ga.jspm.io/npm:morphdom@2.6.1/dist/morphdom.js"
