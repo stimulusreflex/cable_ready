4793820dde4d7846eeafb547b05ce384e4a3cec0
Marco Roth <marco.roth@intergga.ch>
2022-03-18 10:44:15 +0100

    standard

--- rollup.config.js
+++ rollup.config.js
@@ -23,49 +23,50 @@ const minify = () => {
 
 const esConfig = {
   format: 'es',
-  inlineDynamicImports: true,
+  inlineDynamicImports: true
 }
 
 const umdConfig = {
   name: 'CableReady',
   format: 'umd',
   exports: 'named',
-  globals: { morphdom: 'morphdom' },
+  globals: { morphdom: 'morphdom' }
 }
 
-const distFolders = [
-  'dist/',
-  'app/assets/javascripts/'
-]
+const distFolders = ['dist/', 'app/assets/javascripts/']
+
+const output = distFolders
+  .map(distFolder => [
+    {
+      ...esConfig,
+      file: `${distFolder}/cable_ready.js`,
+      plugins: [pretty()]
+    },
+    {
+      ...esConfig,
+      file: `${distFolder}/cable_ready.min.js`,
+      sourcemap: true,
+      plugins: [minify()]
+    },
+    {
+      ...umdConfig,
+      file: `${distFolder}/cable_ready.umd.js`,
+      plugins: [pretty()]
+    },
+    {
+      ...umdConfig,
+      file: `${distFolder}/cable_ready.umd.min.js`,
+      sourcemap: true,
+      plugins: [minify()]
+    }
+  ])
+  .flat()
 
 export default [
   {
     external: ['morphdom'],
     input: 'javascript/index.js',
-    output: distFolders.map(distFolder => [
-      {
-        ...esConfig,
-        file: `${distFolder}/cable_ready.js`,
-        plugins: [pretty()]
-      },
-      {
-        ...esConfig,
-        file: `${distFolder}/cable_ready.min.js`,
-        sourcemap: true,
-        plugins: [minify()]
-      },
-      {
-        ...umdConfig,
-        file: `${distFolder}/cable_ready.umd.js`,
-        plugins: [pretty()]
-      },
-      {
-        ...umdConfig,
-        file: `${distFolder}/cable_ready.umd.min.js`,
-        sourcemap: true,
-        plugins: [minify()]
-      }
-    ]).flat(),
+    output,
     plugins: [commonjs(), resolve(), json()],
     watch: {
       include: 'javascript/**'
