40ec8f5d540b172b04ee78159341a1017b7ecc4e
Julian Rubisch <julian@julianrubisch.at>
2022-05-07 08:38:59 +0200

    Add conditional updates_for helper

--- test/lib/cable_ready/helper_test.rb
+++ test/lib/cable_ready/helper_test.rb
@@ -22,4 +22,20 @@ class CableReady::HelperTest < ActionView::TestCase
     assert_equal "block", element.children.first["class"]
     assert_equal "modal", element.children.first["data-controller"]
   end
+
+  # conditional updates_for
+
+  test "updates_for_if renders if condition is met" do
+    element = Nokogiri::HTML.fragment(updates_for_if(true, "key") { tag.div })
+
+    assert_equal "updates-for", element.children.first.name
+    refute_equal "div", element.children.first.name
+  end
+
+  test "updates_for_if doesn't render if condition isn't met" do
+    element = Nokogiri::HTML.fragment(updates_for_if(false, "key") { tag.div })
+
+    refute_equal "updates-for", element.children.first.name
+    assert_equal "div", element.children.first.name
+  end
 end
