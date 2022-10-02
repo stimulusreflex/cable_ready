cbf9ac932b26caa1f4e554d4562405bfe0cc1053
Marco Roth <marco.roth@intergga.ch>
2022-10-02 06:01:23 +0200

    rename `CableReadyHelper` to `CableReady::Helper`

--- app/helpers/cable_ready/helper.rb
+++ app/helpers/cable_ready/helper.rb
@@ -1,42 +1,44 @@
 # frozen_string_literal: true
 
-module CableReadyHelper
-  include CableReady::Compoundable
-  include CableReady::StreamIdentifier
+module CableReady
+  module Helper
+    include CableReady::Compoundable
+    include CableReady::StreamIdentifier
 
-  def stream_from(*keys, html_options: {})
-    tag.stream_from(**build_options(*keys, html_options))
-  end
-
-  def updates_for(*keys, url: nil, debounce: nil, only: nil, ignore_inner_updates: false, html_options: {}, &block)
-    options = build_options(*keys, html_options)
-    options[:url] = url if url
-    options[:debounce] = debounce if debounce
-    options[:only] = only if only
-    options[:"ignore-inner-updates"] = "" if ignore_inner_updates
-    tag.updates_for(**options) { capture(&block) }
-  end
-
-  def updates_for_if(condition, *keys, **options, &block)
-    if condition
-      updates_for(*keys, **options, &block)
-    else
-      capture(&block)
+    def stream_from(*keys, html_options: {})
+      tag.stream_from(**build_options(*keys, html_options))
     end
-  end
 
-  def cable_car
-    CableReady::CableCar.instance
-  end
+    def updates_for(*keys, url: nil, debounce: nil, only: nil, ignore_inner_updates: false, html_options: {}, &block)
+      options = build_options(*keys, html_options)
+      options[:url] = url if url
+      options[:debounce] = debounce if debounce
+      options[:only] = only if only
+      options[:"ignore-inner-updates"] = "" if ignore_inner_updates
+      tag.updates_for(**options) { capture(&block) }
+    end
 
-  def cable_ready_tag(cable_instance)
-    cable_instance.dispatch(element: true)
-  end
+    def updates_for_if(condition, *keys, **options, &block)
+      if condition
+        updates_for(*keys, **options, &block)
+      else
+        capture(&block)
+      end
+    end
 
-  private
+    def cable_car
+      CableReady::CableCar.instance
+    end
 
-  def build_options(*keys, html_options)
-    keys.select!(&:itself)
-    {identifier: signed_stream_identifier(compound(keys))}.merge(html_options)
+    def cable_ready_tag(cable_instance)
+      cable_instance.dispatch(element: true)
+    end
+
+    private
+
+    def build_options(*keys, html_options)
+      keys.select!(&:itself)
+      {identifier: signed_stream_identifier(compound(keys))}.merge(html_options)
+    end
   end
 end
