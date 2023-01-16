7669812f65f3a52fa06b83ce8648b81de851ca2b
Marco Roth <marco.roth@intergga.ch>
2023-01-16 19:39:23 +0100

    Extract `invoke_method` operation (#239)
    The `invoke_method` operation has been extracted to https://github.com/cableready/invoke_operations

--- javascript/operations.js
+++ javascript/operations.js
@@ -262,43 +262,6 @@ export default {
     })
   },
 
-  invokeMethod: operation => {
-    processElements(operation, element => {
-      before(element, operation)
-      operate(operation, () => {
-        let firstObjectInChain
-        const { element, receiver, method, args } = operation
-        const chain = safeString(method).split('.')
-
-        switch (receiver) {
-          case 'window':
-            firstObjectInChain = window
-            break
-          case 'document':
-            firstObjectInChain = document
-            break
-          default:
-            firstObjectInChain = element
-        }
-        let lastObjectInChain = firstObjectInChain
-        const foundMethod = chain.reduce((lastTerm, nextTerm) => {
-          lastObjectInChain = lastTerm
-          return lastTerm[nextTerm] || {}
-        }, firstObjectInChain)
-
-        if (foundMethod instanceof Function) {
-          foundMethod.apply(lastObjectInChain, args || [])
-        } else {
-          console.warn(
-            `CableReady invoke_method operation failed due to missing '${method}' method for:`,
-            firstObjectInChain
-          )
-        }
-      })
-      after(element, operation)
-    })
-  },
-
   setMeta: operation => {
     before(document, operation)
     operate(operation, () => {
