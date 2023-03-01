2c85734d6ec5987c02f4c35a3c675d7db237bfa9
Julian Rubisch <julian@julianrubisch.at>
2023-03-01 14:38:31 +0100

    chore: Lazy initialize and store collection reflection properties

--- app/models/concerns/cable_ready/updatable/collections_registry.rb
+++ app/models/concerns/cable_ready/updatable/collections_registry.rb
@@ -2,6 +2,17 @@
 
 module CableReady
   module Updatable
+    Collection = Struct.new(
+      :klass,
+      :name,
+      :reflection,
+      :options,
+      :foreign_key,
+      :inverse_association,
+      :through_association,
+      keyword_init: true
+    )
+
     class CollectionsRegistry
       def initialize
         @registered_collections = []
@@ -12,23 +23,35 @@ module CableReady
       end
 
       def broadcast_for!(model, operation)
-        @registered_collections.select { |c| c[:options][:on].include?(operation) }
+        @registered_collections.select { |c| c.options[:on].include?(operation) }
           .each do |collection|
           resource = find_resource_for_update(collection, model)
           next if resource.nil?
 
-          collection[:klass].cable_ready_update_collection(resource, collection[:name], model) if collection[:options][:if].call(resource)
+          collection.klass.cable_ready_update_collection(resource, collection.name, model) if collection.options[:if].call(resource)
         end
       end
 
       private
 
       def find_resource_for_update(collection, model)
-        raise ArgumentError, "Could not find inverse_of for #{collection[:name]}" unless collection[:inverse_association]
+        collection.reflection ||= collection.klass.reflect_on_association(collection.name)
+
+        collection.foreign_key ||= collection.reflection.foreign_key
+
+        # lazy load and store through and inverse associations
+        if collection.reflection&.through_reflection?
+          collection.inverse_association ||= collection.reflection.through_reflection.inverse_of&.name&.to_s
+          collection.through_association = collection.reflection.through_reflection.name.to_s.singularize
+        else
+          collection.inverse_association ||= collection.reflection.inverse_of&.name&.to_s
+        end
+
+        raise ArgumentError, "Could not find inverse_of for #{collection.name}" unless collection.inverse_association
 
         resource = model
-        resource = resource.send(collection[:through_association].underscore) if collection[:through_association]
-        resource.send(collection[:inverse_association].underscore)
+        resource = resource.send(collection.through_association.underscore) if collection.through_association
+        resource.send(collection.inverse_association.underscore)
       end
     end
   end
